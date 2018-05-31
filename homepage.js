efa41f16475b7c571700627477f36ff755cb4f69
ajholsinger <ajholsinger.inc@gmail.com>
2018-05-31 16:25:41 -0400

    tweeked functionality, changed UL to DIV

--- homepage.js
+++ homepage.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
+  //sidebar menu toggle
   $(".icon-button").on("click", function() {
     $(".sidebar").toggleClass("active");
     $(".icon-button").toggleClass("toggleBtnActive");
   });
+  function bigger() {
+  $(this).css("width", "20px", "height", "20px")
+}
 });
